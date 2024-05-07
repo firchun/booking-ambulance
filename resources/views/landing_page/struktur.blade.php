46ba3970bff0ddd9df34f4af6c73b0fdb5067d59
firchun025 <firchun025@gmail.com>
2024-05-07 14:59:40 +0900

    struktur

--- resources/views/landing_page/struktur.blade.php
+++ resources/views/landing_page/struktur.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.landing_page.app')
 @section('content')
-    <section class=" section container my-5">
+    <section class=" section container my-5 text-center">
         <img loading="lazy" class="img-fluid" src="{{ asset('img/struktur.png') }}" alt="service-image">
     </section>
 @endsection
